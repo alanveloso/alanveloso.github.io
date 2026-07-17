290fe48baaf2ea242f9faf5b4f292c0819089383
alantsv <alantsv@gmail.com>
2026-07-17 15:36:52 -0300

    Corrige menu superior no celular.

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,6 +1,7 @@
 /*
 * Greedy Navigation
 *
+* Disabled for the editorial layout: all nav links stay visible and wrap.
 * http://codepen.io/lukejacksonn/pen/PwmwWV
 *
 */
@@ -8,70 +9,18 @@
 var $nav = $('#site-nav');
 var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
-var $vlinks_persist_tail = $vlinks.children("*.persist.tail");
 var $hlinks = $('#site-nav .hidden-links');
 
-var breaks = [];
-
 function updateNav() {
-
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-  // The visible list is overflowing the nav
-  if ($vlinks.width() > availableSpace) {
-
-    while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
-      // Record the width of the list
-      breaks.push($vlinks.width());
-
-      // Move item to the hidden list
-      $vlinks.children("*:not(.persist)").last().prependTo($hlinks);
-
-      availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-      // Show the dropdown btn
-      $btn.removeClass("hidden");
-    }
-
-    // The visible list is not overflowing
-  } else {
-
-    // There is space for another item in the nav
-    while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
-      // Move the item to the visible list
-      if ($vlinks_persist_tail.children().length > 0) {
-        $hlinks.children().first().insertBefore($vlinks_persist_tail);
-      } else {
-        $hlinks.children().first().appendTo($vlinks);
-      }
-      breaks.pop();
-    }
-
-    // Hide the dropdown btn if hidden list is empty
-    if (breaks.length < 1) {
-      $btn.addClass('hidden');
-      $btn.removeClass('close');
-      $hlinks.addClass('hidden');
-    }
-  }
-
-  // Keep counter updated
-  $btn.attr("count", breaks.length);
-
+  // Keep every link in the visible list and hide the overflow toggle.
+  $hlinks.children().appendTo($vlinks);
+  $btn.addClass('hidden').removeClass('close');
+  $hlinks.addClass('hidden');
 }
 
-// Window listeners
-
-$(window).on('resize', function () {
-  updateNav();
-});
-screen.orientation.addEventListener("change", function () {
-  updateNav();
-});
-
-$btn.on('click', function () {
-  $hlinks.toggleClass('hidden');
-  $(this).toggleClass('close');
-});
+$(window).on('resize', updateNav);
+if (screen.orientation && screen.orientation.addEventListener) {
+  screen.orientation.addEventListener('change', updateNav);
+}
 
 updateNav();
